d4d8e355db4b3901a03498e2db7380cf72cf622e
¨Mario-HN¨ <mario_haddad_neto@hotmail.com>
2023-12-11 20:51:41 -0400

    pequenas mudanças e adição de rascunho das tabelas

--- tp1_3.2.py
+++ tp1_3.2.py
@@ -1,19 +1,75 @@
+DB_HOST = "localhost"
+DB_NAME = "Amazon-metadata"
+DB_USER = "admin"
+DB_PASS = "icomp123"
+
 import psycopg2
-def presdb():
+
+def connect_db():
     try:
-        conn = psycopg2.connect(host="localhost",dbname="amazon",user="tb01", password="123")
-        cur = conn.cursor()
-        ##cur.execute()
-#process the result set (cursor)
+        conn = psycopg2.connect(host=DB_HOST, dbname=DB_NAME, user=DB_USER, password=DB_PASS)
+        return conn
+
        
-    except (Exception, psycopg2.DatabaseError) as error :
-        print ("Error while connecting to PostgreSQL", error)
-    #closing database connection.
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Error while connecting to PostgreSQL:", error)
+        return None
+    
+
+def create_tables(conn):
+    try:
+        cursor = conn.cursor()
+
+        cursor.execute("""CREATE TABLE IF NOT EXISTS Produtos(
+            id INT,
+            grupo VARCHAR(30),
+            titulo VARCHAR(1000),
+            salesrank BIGINT,
+            asin VARCHAR(10) NOT NULL,
+            PRIMARY KEY(asin)
+        )""")
+
+        cursor.execute("""CREATE TABLE IF NOT EXISTS Review(
+            asin VARCHAR(10) NOT NULL,
+            data DATE,
+            customer VARCHAR(20),
+            rating INT,
+            novotes INT,
+            nohelpful INT,
+            FOREIGN KEY(asin) REFERENCES Produtos(asin)
+        )""")
+
+        cursor.execute("""CREATE TABLE IF NOT EXISTS Categorias(
+            catid VARCHAR(20),
+            categoria VARCHAR(100)
+        )""")
+
+        cursor.execute("""CREATE TABLE IF NOT EXISTS Prodcat(
+            asin VARCHAR(10),
+            catid VARCHAR(20),
+            FOREIGN KEY(asin) REFERENCES Produtos(asin)
+        )""")
+
+        cursor.execute("""CREATE TABLE IF NOT EXISTS SimilarP(
+            asin VARCHAR(10),
+            similarasin VARCHAR(10),
+            FOREIGN KEY(asin) REFERENCES Produtos(asin)
+        )""")
+
+        conn.commit()
+        print("Tabelas criadas com sucesso.")
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Erro criando tabelas:", error)
+
+    
+if __name__ == '__main__':
+    conn = connect_db()
+
+
     if(conn):
-        cur.close()
+        create_tables(conn)
         conn.close()
         print("PostgreSQL connection is closed")
         
 
-if __name__ == '__main__':
-    presdb()      
+     
