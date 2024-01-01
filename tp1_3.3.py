7325f040f802f854980f39b91802715c9843fbde
¨Mario-HN¨ <mario_haddad_neto@hotmail.com>
2024-01-01 18:57:00 -0400

    atts

--- tp1_3.3.py
+++ tp1_3.3.py
@@ -14,39 +14,44 @@ cursor = conn.cursor()
 
 #a)consuta a listar os 5 comentários mais úteis e com maior avaliação e os 5 comentários mais úteis e com menor avaliação
 cursor.execute("""
-SELECT titulo, rating, nohelpful
+(SELECT titulo, rating, nohelpful
 FROM produtos, review
 WHERE produtos.asin=review.asin
 ORDER BY nohelpful DESC, rating DESC
-LIMIT 5 
-UNION
-SELECT titulo, rating, nohelpful
+LIMIT 5)
+               
+UNION ALL
+               
+(SELECT titulo, rating, nohelpful
 FROM produtos, review
 WHERE produtos.asin=review.asin
 ORDER BY nohelpful DESC, rating ASC
-LIMIT 5;
+LIMIT 5);
 """)
 
 #b)Dado um produto, listar os produtos similares com maiores vendas do que ele
 cursor.execute("""
-SELECT titulo
-FROM Produtos, Similarp
-WHERE Produtos.asin=Similarp.similarasin AND Produtos.salesrank < (
+SELECT p.titulo, asin, 
+FROM Produtos p, Similarp s
+WHERE p.asin=s.asin AND p.salesrank < (
                SELECT salesrank
                FROM Produtos
-               WHERE 'ASIN_DO_PRODUTO'
+               WHERE Produtos.asin='0486220125'
 );
 """)
+
 
 #c)Dado um produto, mostrar a evolução diária das médias de avaliação ao longo do intervalo de tempo coberto no arquivo de entrada
 cursor.execute("""
 SELECT data, AVG(rating)
 FROM Review
-GROUP BY data;
+GROUP BY data
+ORDER BY data;
 """)
 
 #d)Listar os 10 produtos líderes de venda em cada grupo de produtos
 cursor.execute("""
+
 """)
 
 #e)Listar os 10 produtos com a maior média de avaliações úteis positivas por produto
